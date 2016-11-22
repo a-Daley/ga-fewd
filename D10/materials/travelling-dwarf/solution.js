d763dba172d5bc9e718e1676272ee8d5f0678a0b
a-Daley <april@mf-lt-april.local>
2016-11-22 18:36:45 -0500

    Dwarf Homework

---
+++ D10/materials/travelling-dwarf/solution.js
@@ -0,0 +1,68 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+
+var nextMoveX = 0
+
+while (nextMoveX < 4) {
+ d.move()
+ nextMoveX = nextMoveX + 1;
+}
+
+d.orient('down');
+
+var nextMoveY = 0;
+
+while (nextMoveY < 16)  {
+	d.move()
+	nextMoveY = nextMoveY + 1;
+}
+
+d.orient('right');
+
+var nextMoveX = 0
+
+while (nextMoveX < 6) {
+ d.move()
+ nextMoveX = nextMoveX + 1;
+}
+
+d.orient('up');
+
+var nextMoveY = 0;
+
+while (nextMoveY < 11)  {
+	d.move()
+	nextMoveY = nextMoveY + 1;
+}
+
+d.orient('right');
+
+
+var nextMoveX = 0
+
+while (nextMoveX < 6) {
+ d.move()
+ nextMoveX = nextMoveX + 1;
+}
+
+d.orient('down');
+
+var nextMoveY = 0;
+
+while (nextMoveY < 9)  {
+	d.move()
+	nextMoveY = nextMoveY + 1;
+}
+
+d.orient('right');
+
+var nextMoveX = 0
+
+while (nextMoveX < 2) {
+ d.move()
+ nextMoveX = nextMoveX + 1;
+}
+
+
+
+
+
